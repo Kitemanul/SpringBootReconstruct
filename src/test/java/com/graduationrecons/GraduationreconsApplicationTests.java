a63644965c4fa8e5a7d840869536f76176989496
战在月光下 <1525378562@qq.com>
2020-07-29 16:41:59 +0800

    Init

---
+++ src/test/java/com/graduationrecons/GraduationreconsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.graduationrecons;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GraduationreconsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
